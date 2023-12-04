ea51c914fa3fffb74c6890fd4dd51e33a6e5801b
Francisco Bruno <franbruno99@gmail.com>
2023-12-04 19:29:11 -0300

    Add task submit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useState } from 'react'
 import Header from './components/Header'
-import Tasks from './components/Tasks';
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -24,6 +25,13 @@ const App = () => {
     }
 ])
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(
@@ -43,8 +51,9 @@ const App = () => {
   }
 
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks to show')}
     </div>
   )
