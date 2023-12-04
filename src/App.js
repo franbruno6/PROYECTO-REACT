370e2b3399242ef431a9a337830892ad16673369
Francisco Bruno <franbruno99@gmail.com>
2023-12-04 18:42:20 -0300

    Toggle reminder & conditional styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Tasks from './components/Tasks';
 
 const App = () => {
-  const [tasks, setStats] = useState([
+  const [tasks, setTasks] = useState([
     {
         id: 1,
         text: 'Doctors appointment',
@@ -23,10 +23,29 @@ const App = () => {
         reminder: false,
     }
 ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(
+      tasks.filter(
+        (task)=> task.id !== id
+      )
+    )
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map(
+        (task) => task.id === id ? {...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks to show')}
     </div>
   )
 }
